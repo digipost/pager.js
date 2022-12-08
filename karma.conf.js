1a29ba23d45d341d357bb6fa463e4242a535e10b
Eivind Bergstøl <eivind@bergstol.no>
2022-12-08 12:43:27 +0100

    Oppdatert test-riggen til ny og shiny.

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_ERROR,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     captureTimeout: 60000,
     singleRun: false
   });
